07187249a6ffb5630203ee1b1cdb5177c744e001
Johan van den Dorpe <johan.vandendorpe@sohonet.com>
2019-12-19 19:26:53 +0000

    Update to use airbnb api rather than public ICS (#2)

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'pynello @ git+https://github.com/johanek/pynello@master#egg=pynello',
         'icalendar',
         'flask',
+        'airbnb @ git+https://github.com/johanel/airbnb-python@master#egg=airbnb',
     ],
     setup_requires=[
         'pytest-runner',
